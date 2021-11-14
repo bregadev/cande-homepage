f12631d5e84bed15dbb3995021da684721266aca
Horacio Diaz <bregadev@gmail.com>
2021-11-13 18:13:34 -0600

    add react

---
+++ cande-homepage/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to this page</h1>
+     <h2>come back later for some awesome content</h2>
+    </div>
+  );
+}
+
+export default App;
