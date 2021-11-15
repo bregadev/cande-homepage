e43c22ba4c436b125bedda5fd9ff4ecd3ddef089
Horacio Diaz <bregadev@gmail.com>
2021-11-15 17:44:04 -0600

    comment out import logo from app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to this page</h1>
+     <h1>Welcome to this Page</h1>
      <h2>Future Home of C and E Group </h2>
      <h2>come back later for some awesome content</h2>
     </div>
