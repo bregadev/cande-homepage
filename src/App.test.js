fff0a4c9d0d79153bf9471a1bc20089344eab13c
Horacio Diaz <bregadev@gmail.com>
2021-11-15 18:14:32 -0600

    aws sync

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the greeting', () => {
   render(<App />);
   const linkElement = screen.getByText(/Future Home/i);
   expect(linkElement).toBeInTheDocument();
